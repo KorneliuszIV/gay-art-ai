748b02abc9b4d773e4226e00a3a8fa61689c34cd
Jakub Kendziorek <jakub.kednziorek@gmail.com>
2025-05-29 11:36:08 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, session, url_for, g, send_from_directory
+from flask_session import Session
+import dotenv 
+from database import get_db, close_db
+from functools import wraps
+import os
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
+app.config["SECRET_KEY"] = "whatevr"
+app.config["SESSION_PERNAMENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+submitted_ips = set()
+def get_client_ip():
+    if request.headers.getlist("X-Forwarded-For"):
+        return request.headers["X-Forwarded-For"].split(",")[0].strip()
+    return request.remote_addr
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        user_ip = get_client_ip()
+        print('-------', user_ip, '-------------')
+        submitted_ips.add(user_ip)
+        # if user_ip in submitted_ips:
+        #     return "You have already submitted a rating!", 403
+        print(submitted_ips)
+        return render_template('index.html')
+    x1 = request.form.get('art1')
+    x2 = request.form.get('art2')
+    x3 = request.form.get('art3')
+    x4 = request.form.get('art4')
+    x5 = request.form.get('art5')
+    x6 = request.form.get('art6')
+    x7 = request.form.get('art7')
+    x8 = request.form.get('art8')
+    x9 = request.form.get('art9')
+    x10 = request.form.get('art10')
+    sex = request.form.get('sex')
+    
+    data_str = x1+ x2+x3+x4+x5+x6+x7+x8+x9+x10+sex
+    print(data_str)
+    with open('graph_data.txt', 'w') as file:
+        file.write(data_str)
+    
+    db = get_db()
+    db.execute('''INSERT INTO data (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,y) VALUES(?,?,?,?,?,?,?,?,?,?,?);''', (x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,sex))
+    db.commit()
+    return render_template('response.html')
+
+
+
+
+@app.route("/level_data")
+def level_data():
+    return send_from_directory('.', 'graph_data.txt')
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/script.js
@@ -0,0 +1,47 @@
+function updateRatingValue(val, num) {
+    document.getElementById(`ratingValue${num}`).textContent = val;
+}
+
+// Initialize with the default value
+document.addEventListener('DOMContentLoaded', init);
+
+const API_HOSTNAME = 'http://127.0.0.1:5000/';
+async function getData() {
+    const data = await fetch(API_HOSTNAME + `/level_data`)
+    const j = await data.text()
+    return j
+}
+
+
+async function init(){
+    const sliders = document.getElementsByClassName('slider');
+    for (let slider of sliders){
+        document.getElementById('ratingValue').textContent = slider.value;
+    }
+    data = await getData();
+    x_values = [1,2,3,4,5,6,7,8,9,10]
+    y_values = []
+    for (let value of data.slice(0, -1)){
+        y_values.push(value)
+    }
+    console.log(y_values)
+    const myChart = new Chart("canvas", {
+        type: "line",
+        data: {
+            labels: x_values,
+            datasets: [{
+                backgroundColor:"rgba(0,0,255,1.0)",
+                borderColor: "rgba(0,0,255,0.1)",
+                fill: false,
+                data: y_values
+            }]
+        },
+        options: {
+            legend: {display: false},
+            scales: {
+              yAxes: [{ticks: {min: 0, max:10}}],
+            }
+          }
+      });   
+};
+
